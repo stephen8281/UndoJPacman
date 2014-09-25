4d811b061a4c1d5845e910fd99e8ebe58ef747ff
Jason Jang <jcj83429@gmail.com>
2014-09-25 12:59:52 -0700

    added UndoablePacman extends MainUI

---
+++ JPacman-undo/src/main/java/org/jpacman/undo/UndoablePacman.java
@@ -0,0 +1,14 @@
+package org.jpacman.undo;
+
+import org.jpacman.framework.ui.MainUI;
+
+public class UndoablePacman extends MainUI {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Causes the game to roll back to the state before the last player move
+	 */
+	public void undo() {
+	}
+}
