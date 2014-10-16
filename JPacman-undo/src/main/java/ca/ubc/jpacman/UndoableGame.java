b6dc3d72f227610996101ca523b9bfc7a77ad88d
Jason Jang <jcj83429@gmail.com>
2014-10-16 10:45:35 -0700

    getting the UndoableGame to push stuff to the stack. yuck this is ugly

--- JPacman-undo/src/main/java/ca/ubc/jpacman/UndoableGame.java
+++ JPacman-undo/src/main/java/ca/ubc/jpacman/UndoableGame.java
@@ -4,21 +4,24 @@ import java.util.Stack;
 
 import org.jpacman.framework.model.Direction;
 import org.jpacman.framework.model.Game;
-import org.jpacman.framework.model.Player;
+import org.jpacman.framework.model.IBoardInspector.SpriteType;
 import org.jpacman.framework.model.Sprite;
+import org.jpacman.framework.model.Tile;
 
 public class UndoableGame extends Game {
 
 	private Stack<UndoStackFrame> undoStack = new Stack<UndoStackFrame>();
 
 	public void movePlayer(Direction dir) {
-		undoStack.push(new UndoStackFrame(getPlayer(), getGhosts(), false));
-		super.movePlayer(dir);
-		System.out.println("mov");
-	}
-
-	private void eatFood(Player player, Sprite currentSprite) {
-		System.out.println("eat");
+		Tile target = getBoard().tileAtDirection(getPlayer().getTile(), dir);
+		Sprite currentOccupier = target.topSprite();
+		if ((currentOccupier == null || currentOccupier.getSpriteType() != SpriteType.WALL)
+		        && getPlayer().isAlive()) {
+			undoStack.push(new UndoStackFrame(getPlayer(), getGhosts(), currentOccupier != null
+			        && currentOccupier.getSpriteType() == SpriteType.FOOD));
+			super.movePlayer(dir);
+			System.out.println("mov");
+		}
 	}
 
 	/**
